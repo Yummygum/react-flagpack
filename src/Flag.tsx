2ae1cf976798b2742ddcf8d10127f5e755d398ce
info@noudadrichem.com <info@noudadrichem.com>
2019-05-21 12:01:48 +0200

    component is ready

--- src/Flag.tsx
+++ src/Flag.tsx
@@ -2,17 +2,40 @@ import * as React from 'react'
 import './Flag.scss'
 
 interface Props {
-  code: string,
+  code: String,
+  size?: String,
+  customBorderRadius?: String
+  dropshadow?: Boolean,
+  hasBorder?: Boolean,
+  gradient?: String,
+  hasBorderRadius?: Boolean,
 }
 
-function Flag(props: Props) {
-  return (
-    <div>
-      <ul>
-        <li>code: { props.code }</li>
-      </ul>
-    </div>
-  )
+class Flag extends React.PureComponent<Props> {
+  public render() {
+    const {
+      code,
+      gradient = '',
+      size = 'l',
+      hasBorder = false,
+      dropshadow = false,
+      hasBorderRadius = true,
+      // customBorderRadius,
+    } = this.props
+
+    return (
+      <div className={
+        `flag
+        ${gradient}
+        size-${size}
+        ${hasBorder ? 'border' : ''}
+        ${dropshadow ? 'dropshadow' : ''}
+        ${hasBorderRadius ? 'border-radius' : ''}`
+      }>
+        <img src={`https://raw.githubusercontent.com/Yummygum/flag-pack-core/master/svg/${size.toLowerCase()}/${code.toUpperCase()}.svg?sanitize=true`}/>
+      </div>
+    )
+  }
 }
 
 export default Flag
